9325ad0610253baea1fe93b991fa8e533402d828
limerentfox <christi@limerentfox.com>
2017-01-18 16:24:49 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,30 @@
 // declare your functions here...
+$(run)
+
+function run() {
+  paragraphSelector()
+  lastImageSelector()
+  ninjaBabySelector()
+  divSelector()
+  firstListItem()
+}
+
+  function paragraphSelector(){
+    return $('p')
+  }
+
+  function lastImageSelector(){
+    return $('img:last')
+  }
+
+  function ninjaBabySelector(){
+    return $('img#baby-ninja')
+  }
+
+  function divSelector(){
+    return $('div.pics')
+  }
+
+  function firstListItem(){
+    return $('ul li').first()
+  }
